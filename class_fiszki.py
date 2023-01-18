bad52b710dd823f32c4f753159a40023b1143ca1
DESKTOP-9T2SKPR\SU <sofianasekajlo4@gmail.com>
2023-01-18 01:16:12 +0100

    All logic and all tests are ready

--- class_fiszki.py
+++ class_fiszki.py
@@ -20,6 +20,26 @@ class Card:
     def __str__(self):
         return self._word
 
+    @property
+    def word(self):
+        return self._word
+
+    @property
+    def translate(self):
+        return self._translate
+
+    @property
+    def level(self):
+        return self._level
+    
+    @property
+    def repeat(self):
+        return self._repeat
+
+    def __eq__(self, __o: object) -> bool:
+        if (self._word==__o._word and self._translate==__o._translate and self._level == __o._level):
+            return True
+
 #функция для записи ошибочных слов в отдельных файл для повторения
 class Round:
     def __init__(self, number_cards, time):
@@ -27,6 +47,14 @@ class Round:
             Конструктор раунда принимает список фишек и время выполнения теста"""
         self._time = time
         self._number_cards = number_cards
+
+    @property
+    def number_of_cards(self):
+        return self._number_cards
+
+    @property
+    def time(self):
+        return self._time
     
     def check_result(self, sorted_cards, sorted_translate, time):
         """Два критерия по оценке теста:
@@ -38,8 +66,7 @@ class Round:
         bad_words = []
         # good_words = []
         if time > self._time:
-            bad_words = sorted_cards
-            return 
+            return sorted_cards
         for pair_card in zip(sorted_cards, sorted_translate):
             if pair_card[0]._translate != pair_card[1]._word:
                 bad_words.append(pair_card[0])
@@ -52,35 +79,50 @@ class Round:
         all_lines = []
         with open(file, 'r+') as f:
             all_lines = f.readlines()
-            f.seek(0)
-            f.truncate()
+            # f.seek(0)
+            # f.truncate()
         all_words = []
         for line in all_lines:
-            word, translate, level, time_repeat = line.split('-')
-            card = Card(word, translate, level, time_repeat)
+            parts = line.split('-')
+            word, translate, level, repeat = parts[0], parts[1], parts[2], parts[3].rstrip(),
+            card = Card(word, translate, int(level), int(repeat))
             all_words.append(card)
         for card in all_words:
             if card in words:
-                card._repeat = 2 * card._repeat
+                card._repeat = 4 * card._repeat
             else:
+                if card._repeat == 1:
+                    continue
                 card._repeat = card._repeat / 2
-        all_words.sort(key=lambda x: x._repeat)
-        with open(file, 'w') as f:
-            for card in all_words:
-                f.write(f'{card._word}-{card._translate}-{card._level}-{card._repeat}\n')
+        # all_words.sort(key=lambda x: x._repeat)
+        # with open(file, 'w') as f:
+        #     f.write("".join(all_words))
+        return all_words
 
         
 
-    def add_new_card(self, word, translate):
+    def add_new_card(self, word, translate, level):
         """DELETE AT THE END"""
         card = Card(word, translate)
         self.append(card)
 
     def export_card(self, card: Card, file=database):
         """Експорт принимает фишку, которую нужно экспортировать
-            и файл, в который нужно ее сохранить(файл определен как константа"""
-        with open(file, 'w') as f:
-            f.write(f'{card._word}-{card._translate}-{card._level}-{card._repeat}\n')
+            и файл, в который нужно ее сохранить(файл определен как константа)"""
+        with open(file, 'r+') as f:
+            all_lines = f.readlines()
+            f.seek(0)
+            f.truncate()
+            for line in all_lines:
+                parts = line.split('-')
+                it_word, it_translate, it_level, it_repeat = parts[0], parts[1], parts[2], parts[3]
+                if (it_word == card.word):
+                    f.write("".join(all_lines))
+                    raise ValueError
+            all_lines.append(f'{card._word}-{card._translate}-{card._level}-{card._repeat}\n')
+            # for line in all_lines:
+            #     f.write(line)
+            f.write("".join(all_lines))
 
     def import_card(self, cards, file=database):
         """Импорт принимает список разыгрываемых карточек и файл, их которого импортируется фишка.
@@ -89,35 +131,38 @@ class Round:
             3. Если такого слова нет в данном тесте, оно импортируется
             4. Если попали в слово, которое уже есть в тесте, выбираем еще иаз рандомное слово
             5. И так до тех пор пока не найдем неповторяющиеся слово"""
-        with open(file, 'w') as f:
-            line_count = sum(1 for line in f)
-            random_line = randint(1, line_count)
-            pair = f.readline(random_line)
-            word, translate, level, repeat = pair.split('-')
-            card = Card(word, translate, level, repeat)
+        with open(file, 'r') as f:
+            all_lines = f.readlines()
+            line_count = len(all_lines)
+            random_line = randint(0, line_count-1)
+            pair = all_lines[random_line]
+            parts = pair.split('-')
+            word, translate, level, repeat = parts[0], parts[1], parts[2], parts[3].rstrip(), 
+            card = Card(word, translate, int(level))
             while card in cards:
-                random_line = randint(1, line_count)
-                pair = f.readline(random_line)
-                word, translate, level, repeat = pair.split('-')
-                card = Card(word, translate, level, repeat)
+                random_line = randint(0, line_count-1)
+                pair = all_lines[random_line]
+                parts = pair.split('-')
+                word, translate, level, repea0 = parts[0], parts[1], parts[2], parts[3].rstrip()
+                card = Card(word, translate, int(level))
             cards.append(card)
+            return cards
 
-    def define_new_card(self, word, translate, level: int, file = database):
+    def define_new_card(self, word, translate, level: int, cards):
         """Добавление новой карточки вручную.
            На вход принимается слово, перевод, уровень сложности и файл для записи
             1. Считываются все уже существующиеся из файла
             2. Проверка: если такое слово уже есть, выходим из функции
             3. Если нет, добавляем новое слово
            Тут исключено добавление одного и того же слова с разным уровнем сложности."""
-        all_words = []
-        with open(file, 'w') as f:
-            all_words = f.readlines()
-            card = f'{word}-{translate}-{level}-{repeat_time}'
-            for line in all_words:
-                iterate_word, iterate_translate, iterate_level = line.split('-')
-                if (iterate_word == word):
-                    raise ValueError
-            f.write(f'{card}\n')
+        if (word == "" or translate == ""):
+            raise ValueError
+        for card in cards:
+            if (card.word == word):
+                raise ValueError
+        card = Card(word, translate, level)
+        cards.append(card)
+        return cards
 
     def delete_card(self, card: Card, file = database): 
         """На вход принимаются сама фишка и файл, из которого будет удалять слово
@@ -127,17 +172,24 @@ class Round:
             4. Если встретили ту которую нужно удалить, просто пропускаем"""
         all_words = []
         with open(file, 'r+') as f:
-            all_words = f.readlines()
+            all_lines = f.readlines()
             word = f'{card._word}-{card._translate}-{card._level}-{card._repeat}'
             f.seek(0)
             f.truncate()
+            new_words = []
             flag = 0
-            for line in all_words:
-                if line.strip("\n") != word:
-                    f.write(word+"\n")
+            for line in all_lines:
+                parts = line.split('-')
+                word, translate, level, repeat = parts[0], parts[1], parts[2], parts[3].rstrip(), 
+                if word == card._word:
                     flag = 1
+                    continue
+                else:
+                    new_words.append(line)
+            f.write("".join(new_words))
             if flag == 0:
                 raise ValueError
+            return new_words
     
     def choose_random_cards(self, file = database):
         test_cards = []
@@ -145,20 +197,20 @@ class Round:
             all_words = f.readlines()
         repeat_words = []
         for line in all_words:
-            word, translate, level, repeat = line.split('-')
-            if repeat != 2:
-                break
+            parts = line.split('-')
+            word, translate, level, repeat = parts[0], parts[1], parts[2], parts[3].rstrip(), 
+            if int(repeat) != 1:
+                continue
             else:
                 repeat_words.append(line)
         test_words = []
-        for i in range(self._number_cards):
-            word, translate, level, repeat = repeat_words[i].split('-')
-            card = Card(word, translate, level, repeat)
+        for i in range(0, self._number_cards):
+            parts = repeat_words[i].split('-')
+            word, translate, level, repeat = parts[0], parts[1], parts[2], parts[3].rstrip(), 
+            card = Card(word, translate, level)
             test_words.append(card)
         return test_words
 
-    def play_round(self, file = database):
-        """В процессе разработки (сначала дописать генерацию карточек и их запись"""
         
 
 
---
+++ test_class_fiszki.py
@@ -0,0 +1,167 @@
+from class_fiszki import *
+import pytest
+database = 'words_for_repeat.txt'
+
+def test_init_card():
+    card = Card("cześć", "hello", 1)
+    assert card.word == "cześć"
+    assert card.translate == "hello"
+    assert card.level == 1
+    assert card.repeat == 8
+
+def test_error_card():
+    with pytest.raises(ValueError):
+        card = Card("", "hello", 1)
+        assert card.word == "cześć"
+
+def test_round_init():
+    number_of_cards = 10
+    time = 60
+    round = Round(number_of_cards, time)
+    assert round.number_of_cards == 10
+    assert round.time == 60
+
+def test_export_card():
+    card = Card("broskwina", "peach", 3)
+    round = Round(5, 60)
+    round.export_card(card, "new_test.txt")
+    with open("new_test.txt", 'r') as f:
+        f.readline()
+        f.readline()
+        f.readline()
+        str = f.readline()
+    assert str == f'broskwina-peach-3-8\n'
+
+def test_export_exist_card():
+    with pytest.raises(ValueError):
+        card = Card("broskwina", "peach", 3)
+        round = Round(5, 60)
+        round.export_card(card, "new_test.txt")
+        with open("new_test.txt", 'r') as f:
+            f.readline()
+            f.readline()
+            f.readline()
+            str = f.readline()
+
+def test_import_card():
+    card1 = Card("jablko", "apple", 2)
+    card2 = Card("siema", "bye", 1)
+    cards = [card1, card2]
+    round = Round(5, 60)
+    new_cards = round.import_card(cards)
+    expected_card = Card('truskawka', 'strawberry', 3)
+    assert new_cards[2].word == 'truskawka'
+
+def test_define_new_card():
+    card1 = Card("jablko", "apple", 2)
+    card2 = Card("siema", "bye", 1)
+    cards = [card1, card2]
+    round = Round(5, 60)
+    card3 = Card("malpa", "monkey", 3)
+    new_cards = round.define_new_card("malpa", "monkey", 3, cards)
+    assert new_cards[2] == card3
+
+def test_define_exist_card():
+    with pytest.raises(ValueError):
+        card1 = Card("jablko", "apple", 2)
+        card2 = Card("siema", "bye", 1)
+        card3 = Card("malpa", "monkey", 3)
+        cards = [card1, card2, card3]
+        round = Round(5, 60)
+        round.define_new_card("malpa", "monkey", 3, cards)
+
+def test_define_card_empty_word():
+    with pytest.raises(ValueError):
+        card1 = Card("jablko", "apple", 2)
+        card2 = Card("siema", "bye", 1)
+        card3 = Card("malpa", "monkey", 3)
+        cards = [card1, card2, card3]
+        round = Round(5, 60)
+        round.define_new_card("", "monkey", 3, cards)
+
+def test_define_card_empty_trans():
+    with pytest.raises(ValueError):
+        card1 = Card("jablko", "apple", 2)
+        card2 = Card("siema", "bye", 1)
+        card3 = Card("malpa", "monkey", 3)
+        cards = [card1, card2, card3]
+        round = Round(5, 60)
+        round.define_new_card("malpa", "", 3, cards)
+
+def test_delete_card():
+    card2 = Card("siema", "bye", 1)
+    round = Round(5, 60)
+    new_words = round.delete_card(card2)
+    assert len(new_words) == 3
+
+def test_delete_not_existing_card():
+    with pytest.raises(ValueError):
+        card = Card("dom", "house", 1)
+        round = Round(5, 60)
+        new_words = round.delete_card(card)
+        assert len(new_words) == 4
+
+def test_random_cards():
+    round = Round(6, 60)
+    random_words = round.choose_random_cards()
+    assert random_words[0].word == 'truskawka'
+    assert random_words[4].word == 'szczotka'
+    assert random_words[5].word == "szkola"
+
+def test_check_result_true():
+    card1 = Card("jablko", "apple", 2)
+    card2 = Card("siema", "bye", 1)
+    card3 = Card("malpa", "monkey", 3)
+    card4 = Card("szczotka", "hairbrush", 2)
+    tr_card1 = Card("apple", "jablko", 2)
+    tr_card2 = Card("bye", "siema", 1)
+    tr_card3 = Card("monkey", "malpa", 3)
+    tr_card4 = Card("hairbrush", "szczotka", 2)
+    sorted_cards = [card1, card2, card3, card4]
+    sorted_translate = [tr_card1, tr_card2, tr_card3, tr_card4]
+    round = Round(4, 60)
+    bad_words = round.check_result(sorted_cards, sorted_translate, 50)
+    assert bad_words == []
+
+def test_check_result_false():
+    card1 = Card("jablko", "apple", 2)
+    card2 = Card("siema", "bye", 1)
+    card3 = Card("malpa", "monkey", 3)
+    card4 = Card("szczotka", "hairbrush", 2)
+    tr_card1 = Card("apple", "jablko", 2)
+    tr_card2 = Card("bye", "siema", 1)
+    tr_card3 = Card("monkey", "malpa", 3)
+    tr_card4 = Card("hairbrush", "szczotka", 2)
+    sorted_cards = [card1, card2, card3, card4]
+    sorted_translate = [tr_card2, tr_card1, tr_card3, tr_card4]
+    round = Round(4, 60)
+    bad_words = round.check_result(sorted_cards, sorted_translate, 50)
+    assert bad_words == [card1, card2]
+
+def test_check_result_more_time():
+    card1 = Card("jablko", "apple", 2)
+    card2 = Card("siema", "bye", 1)
+    card3 = Card("malpa", "monkey", 3)
+    card4 = Card("szczotka", "hairbrush", 2)
+    tr_card1 = Card("apple", "jablko", 2)
+    tr_card2 = Card("bye", "siema", 1)
+    tr_card3 = Card("monkey", "malpa", 3)
+    tr_card4 = Card("hairbrush", "szczotka", 2)
+    sorted_cards = [card1, card2, card3, card4]
+    sorted_translate = [tr_card1, tr_card2, tr_card3, tr_card4]
+    round = Round(4, 60)
+    bad_words = round.check_result(sorted_cards, sorted_translate, 70)
+    assert bad_words == [card1, card2, card3, card4]
+
+def test_words_for_repeat():
+    card1 = Card("jablko", "apple", 2, 8)
+    card2 = Card("siema", "bye", 1, 4)
+    card3 = Card("malpa", "monkey", 2, 4)
+    card4 = Card("szczotka", "hairbrush", 2, 1)
+    cards = [card1, card2, card3, card4]
+    round = Round(4, 60)
+    update_words = round.words_for_repeat(cards)
+    assert update_words[0].repeat == 32
+    assert update_words[1].repeat == 16
+    assert update_words[2].repeat == 1
+    assert update_words[11].repeat == 16
